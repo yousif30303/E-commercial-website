f2ad0a27b013e5e6d2b4246ca3d3136b40b9fc67
yousif30303 <yousif30303@gmail.com>
2020-12-13 08:08:19 -0800

    fetching the product

--- functions.php
+++ functions.php
@@ -3,8 +3,17 @@
 // require MySQL Connection 
 require('database/DBController.php');
 
+// require Product Class
+require ('database/Product.php');
+
+
 //DBController object
 $db = new DBController();
+
+// Product object
+$product = new Product($db);
+
+print_r($product->getData());
 
 
 
