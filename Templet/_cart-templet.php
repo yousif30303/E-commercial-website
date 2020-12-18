f1eb6c0b83f0000ed93a7e6ea8d45c9185da6b9b
yousif30303 <yousif30303@gmail.com>
2020-12-18 00:34:17 -0800

    calculate the sum of cart items price

--- Templet/_cart-templet.php
+++ Templet/_cart-templet.php
@@ -9,7 +9,8 @@
                                 <?php
                                     foreach ($product->getData('cart') as $item) :
                                     $cart = $product->getProduct($item['item_id']);
-                                    array_map(function($item){
+                                    $subTotal[]= array_map(function($item){
+                                      
                                 ?>
                                     <!-- cart item -->
                                         <div class="row border-top py-3 mt-3">
@@ -54,6 +55,7 @@
                                         </div>
                                     <!-- !cart item -->
                                     <?php
+                                    return $item['item_price'];
                                     },$cart);//closing array_map function
                                     endforeach;
                                     ?>
@@ -63,7 +65,7 @@
                                     <div class="sub-total border text-center mt-2">
                                         <h6 class="font-size-12 font-rale text-success py-3"><i class="fas fa-check"></i> Your order is eligible for FREE Delivery.</h6>
                                         <div class="border-top py-4">
-                                            <h5 class="font-baloo font-size-20">Subtotal (2 item):&nbsp; <span class="text-danger">$<span class="text-danger" id="deal-price">152.00</span> </span> </h5>
+                                            <h5 class="font-baloo font-size-20">Subtotal (<?php echo count($subTotal) ?? 0; ?> item):&nbsp; <span class="text-danger">$<span class="text-danger" id="deal-price"><?php echo isset($subTotal) ? $Cart->getSum($subTotal) : 0; ?></span> </span> </h5>
                                             <button type="submit" class="btn btn-warning mt-3">Proceed to Buy</button>
                                         </div>
                                     </div>
